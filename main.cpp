5dd0e8ec3f7263ce9df7d7ee9bd86104ec5c5715
ShihaleevVladimir <vovashih1@gmail.com>
2024-11-20 17:02:39 +0500

    Сложение 2-х чисел

--- main.cpp
+++ main.cpp
@@ -1,3 +1,25 @@
+// This program is just an example one under VCS.
+// Author: Shichaleev Vladimir, KE-217
+//
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
 int main()
 {
+	setlocale(LC_ALL, "Russian"); 
+
+	int a, b, c;
+
+	cout << "Pervoe chislo" << endl;
+	cin >> a;
+
+	cout << "Vtoroe chislo" << endl;
+	cin >> b;
+
+	c = a + b;
+
+	cout << "PLUS = " << c << endl;
+
 }
